f34e8a2485cda7411f7c67bb8b1bede3900e69bc
Mihaela Robb <mihaela.robb@gmail.com>
2019-04-28 18:53:38 -0700

    Consume Web API
    Make async call

--- BHDemo/StockMVC/Controllers/HomeController.cs
+++ BHDemo/StockMVC/Controllers/HomeController.cs
@@ -1,18 +1,34 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
+using StockMVC.Helper;
+using StockMicroservice.Models;
+using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using StockMVC.Models;
+using System.Diagnostics;
 
 namespace StockMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        StockAPI stockAPI = new StockAPI();
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Stock> stocks = new List<Stock>();
+            HttpClient client = stockAPI.Initialize();
+            HttpResponseMessage resp = await client.GetAsync("https://localhost:44339/api/stocks");
+            if (resp.IsSuccessStatusCode)
+            {
+                var result = resp.Content.ReadAsStringAsync().Result;
+                stocks = JsonConvert.DeserializeObject<List<Stock>>(result);
+            }
+
+            return View(stocks);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ BHDemo/StockMVC/Helper/StockAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace StockMVC.Helper
+{
+    public class StockAPI
+    {
+        public HttpClient Initialize()
+        {
+            var Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44339/api/stock");
+            return Client;
+        }
+
+    }
+}
